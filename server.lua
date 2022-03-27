901bb4f29b730f3f980d3e982b4531d08947b487
ffpetrozzi <51758256+ffpetrozzi@users.noreply.github.com>
2022-03-27 15:42:14 +0200

    Delete server.lua

--- server.lua
+++
@@ -1,6 +0,0 @@
-ESX = nil
-TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
-
-ESX.RegisterServerCallback('ff_peds:GetSteamHex', function(source, cb)
-	cb(GetPlayerIdentifiers(source)[1])
-end)
