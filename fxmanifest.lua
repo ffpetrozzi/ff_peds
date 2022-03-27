94f189db4865919cb4f5caea31f6509688218e99
ffpetrozzi <51758256+ffpetrozzi@users.noreply.github.com>
2022-03-27 15:39:37 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,5 +1,12 @@
-fx_version 'adamant'
+fx_version 'cerulean'
+
 game 'gta5'
+
+lua54 'yes'
+
+author 'ffpetrozzi | https://github.com/ffpetrozzi'
+
+version '0.0.2'
 
 client_scripts {
 	'config.lua',
