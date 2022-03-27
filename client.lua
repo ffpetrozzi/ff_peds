279f375dfc937fe64b4d68f1fa2f0c109c1943cd
ffpetrozzi <51758256+ffpetrozzi@users.noreply.github.com>
2022-03-27 15:41:29 +0200

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,32 +1,29 @@
-ESX = nil
+ESX = exports.es_extended:getSharedObject()
 
-Citizen.CreateThread(function()
-	while ESX == nil do
-		TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
-		Citizen.Wait(0)
-	end
+RegisterNetEvent('esx:playerLoaded')
+AddEventHandler('esx:playerLoaded', function(xPlayer)
+	ESX.PlayerLoaded = true
+	ESX.PlayerData = xPlayer
 end)
 
 RegisterCommand(Config.CommandName, function(source)
 	for k, v in pairs(Config.Peds) do
-		ESX.TriggerServerCallback('ff_peds:GetSteamHex', function(steamhexplayer)
-			if steamhexplayer == v.steamhex then
-				if not v.pedmenu then
-					SetPed(v.ped)
-				elseif v.pedmenu then
-					ESX.UI.Menu.Open('dialog', GetCurrentResourceName(), 'chooseped', {
-						title = Config.Lang["chooseaped"]
-					}, function(data, menu)
-						menu.close()
-						SetPed(data.value)
-					end, function(data, menu)
-						menu.close()
-					end)
-				end
-			elseif not steamhexplayer == v.steamhex and Config.EnableNoPermsNotification then
-				ESX.ShowNotification(Config.Lang["noperms"])
+		if ESX.PlayerData.identifier == v.steamhex then
+			if not v.pedmenu then
+				SetPed(v.ped)
+			elseif v.pedmenu then
+				ESX.UI.Menu.Open('dialog', GetCurrentResourceName(), 'chooseped', {
+					title = Config.Lang["chooseaped"]
+				}, function(data, menu)
+					menu.close()
+					SetPed(data.value)
+				end, function(data, menu)
+					menu.close()
+				end)
 			end
-		end)
+		elseif not ESX.PlayerData.identifier == v.steamhex and Config.EnableNoPermsNotification then
+			ESX.ShowNotification(Config.Lang["noperms"])
+		end
 	end
 end, false)
 
